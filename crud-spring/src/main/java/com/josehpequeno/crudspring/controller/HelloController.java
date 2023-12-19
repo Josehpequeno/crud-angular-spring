943c0098daa5e08718b46ffdb82a654a89ef15b4
Josehpequeno <hicarojbs21@gmail.com>
2023-12-19 11:40:58 -0300

    init java spring project

---
+++ crud-spring/src/main/java/com/josehpequeno/crudspring/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.josehpequeno.crudspring.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping("/api/hello")
+public class HelloController {
+
+  @GetMapping
+  public String hello() {
+    return "Hello, World!";
+  }
+
+}
